20f7ab3d9b2036e26917acb529eb3c2fba7a47ea
OpenCV <peterhuang>
2017-12-17 10:43:01 +0800

    [Update]Finish FourierTransform and switch phase

--- Fourier Transform/FourierTransformBK.cpp
+++ Fourier Transform/FourierTransformBK.cpp
@@ -4,58 +4,52 @@
 
 using namespace cv;
 
+
+
 int main(){
-  Mat inputImg = imread("Inputimage1.png", CV_LOAD_IMAGE_GRAYSCALE);
-  Mat inputImg2 = imread("selfy5.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-  
-  //OptimalDFT
-   Mat padded;
-   int m = getOptimalDFTSize(inputImg.rows); 
-   int n = getOptimalDFTSize(inputImg.cols);
-   copyMakeBorder(inputImg, padded, 0, m-inputImg.rows, 0, n-inputImg.cols, BORDER_CONSTANT, Scalar::all(0)); 
-   
-   //OptimalDFT
+    Mat inputImg = imread("Inputimage1.png", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat inputImg2 = imread("selfy5.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+    Mat padded;
+    int m = getOptimalDFTSize(inputImg.rows); 
+    int n = getOptimalDFTSize(inputImg.cols);
+    copyMakeBorder(inputImg, padded, 0, m-inputImg.rows, 0, n-inputImg.cols, BORDER_CONSTANT, Scalar::all(0)); 
+
     Mat padded2;
     int m2 = getOptimalDFTSize(inputImg2.rows);  
     int n2= getOptimalDFTSize(inputImg2.cols);
     copyMakeBorder(inputImg2, padded2, 0, m2-inputImg2.rows, 0, n2-inputImg2.cols, BORDER_CONSTANT, Scalar::all(0)); 
 
-    //preallocate
+
     Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
     Mat complexImg;
     merge(planes, 2, complexImg);
-    //getdft
     dft(complexImg, complexImg);
 
-   
     Mat planes2[] = {Mat_<float>(padded2), Mat::zeros(padded2.size(), CV_32F)};
     Mat complexImg2;
     merge(planes2, 2, complexImg2);
-     //getdft2
     dft(complexImg2, complexImg2);
 
-    split(complexImg2,planes2); //分離通道，planes[0]為實數部分，planes[1]為虛數部分
+    split(complexImg2,planes2);
   
     split(complexImg, planes);   //分離通道，planes[0]為實數部分，planes[1]為虛數部分
-    
-    //Save parameter
-    Mat rPart1,iPart1,rPart2,iPart2;
-    planes[0].copyTo(rPart1);
-    planes[1].copyTo(iPart1);
-    planes2[0].copyTo(rPart2);
-    planes2[1].copyTo(iPart2);
-
-    //cal magnitude 
-  
+    Mat oriPhase1;
+    planes[1].copyTo(oriPhase1);
+    Mat originPhase;
+    phase(planes[0],planes[1],originPhase,true);
+    Mat oriPhase2;
+    planes2[1].copyTo(oriPhase2);
+    Mat originalImage1RealPart;
+    planes[0].copyTo(originalImage1RealPart);
+    Mat originalImage2RealPart = planes2[0].clone();
     magnitude(planes[0], planes[1], planes[0]); //planes[0] = sqrt((planes[0])^2 + (planes[1])^2
     magnitude(planes2[0], planes2[1], planes2[0]); //planes[0] = sqrt((planes[0])^2 + (planes[1])^2
-    //Save origmagnitude
-    Mat origmagnitude1 = planes[0].clone();
-    Mat origmagnitude2 = planes2[0].clone();
+    Mat origmagnitude = planes[0].clone();
     Mat magnitude1 = planes[0].clone();//magnitude
     Mat magnitude2 = planes2[0].clone();//magnitude2
-
-    //move to orign
+   
+  
     magnitude1 += Scalar::all(1); //magI = log(1+planes[0])
     magnitude2 += Scalar::all(1);
     log(magnitude1, magnitude1);
@@ -100,63 +94,85 @@ int main(){
 
 
 
-    // normalize to humanreadable
+
     normalize(magnitude1, magnitude1, 0, 1, CV_MINMAX);
-   // normalize to humanreadable
+
     normalize(magnitude2, magnitude2, 0, 1, CV_MINMAX);
 
-    //cal phase1
+    //cal phase
+    Mat magn,angle,oriangle,orimagn;
+    cartToPolar(originalImage2RealPart,oriPhase2,magn,angle,true);
+    angle.copyTo(oriangle);
+   magn.copyTo(orimagn);
     Mat magn1,angle1,oriangle1,orimagn1;
-    cartToPolar(rPart1,iPart1,magn1,angle1,true);
-    //Save angle1
-    angle1.copyTo(oriangle1);
-    //normalize to human readable
-    normalize(angle1, angle1, 255,0);
+    cartToPolar(originalImage1RealPart,oriPhase1,magn1,angle1,true);
+    normalize(angle, angle, 255,0);
+    log(magn, magn);
+    normalize(magn, magn, 0, 1, CV_MINMAX);
+    Mat complexx = magn.mul(angle);
+  
+    //Switch phase
+    Mat newplanes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+    //newplanes[0]= originalImage1RealPart.clone();
+    //magn.copyTo(newplanes[0]);
+    oriangle.copyTo(newplanes[1]);
+    Mat newplanes2[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+    originalImage2RealPart.copyTo(newplanes2[0]);
+    oriPhase1.copyTo(newplanes2[1]);
+    Mat newcomplexImg;
+    newcomplexImg = magn.mul(oriangle);
+    // merge(newplanes,2, newcomplexImg);
 
-    //cal phase2
-    Mat magn2,angle2,oriangle2,orimagn2;
-    cartToPolar(rPart2,iPart2,magn2,angle2,true);
-    //Save angle2
-    angle2.copyTo(oriangle2);
-    //normalize to human readable
-    normalize(angle2, angle2, 255,0);
-
-    //Show Result
+     Mat newcomplexImg2;
+    merge(newplanes2,2, newcomplexImg2);
+    /*Mat tempPhase;
+    planes[1].copyTo(tempPhase);
+    planes[1].release();
+    planes[1]=planes2[1].clone();
+    planes2[1].release();
+    planes2[1] = tempPhase.clone();
+    
+    Mat newcomplexImg;
+    merge(newplanes,2, newcomplexImg);
+  
+    merge(planes2, 2, complexImg2);*/
+ normalize(originPhase, originPhase, 255,0);
     imshow("InputImage1", inputImg);
     imshow("InputImage2", inputImg2);
     imshow("mangtidue1", magnitude1);
     imshow("mangtidue2", magnitude2);
-    imshow("Phase1", angle1);
-    imshow("Phase2", angle2);
-
-    //Switch phase
-    Mat newplanes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-    Mat newplanes2[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-
-   //mangtidue1+Phase2;
-    polarToCart(magn1,oriangle2,newplanes[0],newplanes[1],true);
-
-    //mangtidue2+Phase1;
-    polarToCart(magn2,oriangle1,newplanes2[0],newplanes2[1],true);
-
-    Mat newComplex1,newComplex2;
-    merge(newplanes, 2, newComplex1);
-      merge(newplanes2, 2, newComplex2);
-    //Merge
-
+    imshow("Phase1", originPhase);
+    imshow("Phase2", planes2[1]);
+    
+    Mat afterExp;
+    Mat aftermulti;
+    aftermulti = originPhase*sqrt(-1);
+    exp(aftermulti,afterExp);
+    Mat aftermulti2 = origmagnitude.mul(afterExp);
+    Mat newreal,newimage;
+    Mat newComplexxx;
+    polarToCart(orimagn,angle1,newplanes[0],newplanes[1],true);
+    merge(newplanes, 2, newComplexxx);
     //逆向傅立葉轉換
-     Mat ifft;
-    idft(newComplex1,ifft,DFT_INVERSE|DFT_REAL_OUTPUT);
+    Mat ifft;
+    idft(newComplexxx,ifft,DFT_INVERSE|DFT_REAL_OUTPUT);
     
     normalize(ifft,ifft,0,1,CV_MINMAX);
     imshow("mangtidue1+Phase2",ifft);
 
     Mat ifft2;
-    idft(newComplex2,ifft2,DFT_REAL_OUTPUT);
+    idft(complexImg2,ifft2,DFT_REAL_OUTPUT);
     normalize(ifft2,ifft2,0,1,CV_MINMAX);
     imshow("mangtidue2+Phase1",ifft2);
 
-  waitKey(0);
 
-  return 0;
+
+    waitKey(0);
+
+    // Read image from file
+    // Make sure that the image is in grayscale
+
+
+
+    return 0;
 }
